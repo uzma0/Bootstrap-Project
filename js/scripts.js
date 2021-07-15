37b41210396edd9b5b586a832e4f4f468339254a
Uzma Naz <uzamanaz19@gmail.com>
2021-07-14 18:19:44 -0700

    Bootstrap Project

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
+
+
+$(document).ready(function(){
+  $("#reserveButton").click(function(){
+    $("#reserveModal").modal();
+  });
+});
+$(document).ready(function(){
+  $("#loginButton").click(function(){
+    $("#loginModal").modal();
+  });
+});
